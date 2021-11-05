8a7d466fd03e762dfb0d0ef3c143f00fe4656703
csruiliu <rui.liu@proton.me>
2021-11-05 15:07:40 -0500

    format queries

--- queries/q11.py
+++ queries/q11.py
@@ -1,21 +1,21 @@
 query = """
 SELECT	PS_PARTKEY,
-		sum(PS_SUPPLYCOST * PS_AVAILQTY) AS VALUE
+	sum(PS_SUPPLYCOST * PS_AVAILQTY) AS VALUE
 FROM	partsupp,
-		supplier,
-		nation
+	supplier,
+	nation
 WHERE	PS_SUPPKEY = S_SUPPKEY
-		AND S_NATIONKEY = N_NATIONKEY
-		AND N_NAME = 'GERMANY'
+	AND S_NATIONKEY = N_NATIONKEY
+	AND N_NAME = 'GERMANY'
 GROUP BY	PS_PARTKEY HAVING
-			sum(PS_SUPPLYCOST * PS_AVAILQTY) > (
+		sum(PS_SUPPLYCOST * PS_AVAILQTY) > (
     			SELECT	sum(PS_SUPPLYCOST * PS_AVAILQTY) * 0.0001
     			FROM	partsupp,
-						supplier,
-						nation
+				supplier,
+				nation
     			WHERE	PS_SUPPKEY = S_SUPPKEY
-						AND S_NATIONKEY = N_NATIONKEY
-						AND N_NAME = 'GERMANY'
-			)
+				AND S_NATIONKEY = N_NATIONKEY
+				AND N_NAME = 'GERMANY'
+		)
 ORDER BY	VALUE DESC
 """
