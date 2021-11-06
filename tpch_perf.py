c4c9bed0ec058282d3d4e67887ee7294a419acc6
csruiliu <rui.liu@proton.me>
2021-11-06 11:09:03 -0500

    add perf and schama

--- tpch_perf.py
+++ tpch_perf.py
@@ -2,10 +2,12 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from datetime import datetime
 from pathlib import Path
+import argparse
 from time import time
 
-from tpch_schema import schema_lineitem
-from queries import q1
+from tpch_schema import *
+# import all queries in __init__.py in queries folder
+from queries import *
 
 
 def conf_setup():
@@ -26,29 +28,122 @@ def read_table(spark_session, spark_context):
     lineitem = lineitem_split.map(lambda l: [int(l[0]), int(l[1]), int(l[2]), int(l[3]), float(l[4]), float(l[5]), float(l[6]), float(l[7]), str(l[8]), str(
         l[9]), datetime.strptime(l[10], "%Y-%m-%d"), datetime.strptime(l[11], "%Y-%m-%d"), datetime.strptime(l[12], '%Y-%m-%d'), str(l[13]), str(l[14]), str(l[15])])
 
-    # create a dataframe for lineitem
+    part_split = spark_context.textFile(
+        DATA_PATH+"part.tbl").map(lambda l: l.split('|'))
+
+    part = part_split.map(lambda l: [int(l[0]), str(l[1]), str(l[2]), str(
+        l[3]), str(l[4]), int(l[5]), str(l[6]), float(l[7]), str(l[8])])
+
+    supplier_split = spark_context.textFile(
+        DATA_PATH+"supplier.tbl").map(lambda l: l.split('|'))
+
+    supplier = supplier_split.map(lambda l: [int(l[0]), str(l[1]), str(
+        l[2]), int(l[3]), str(l[4]), float(l[5]), str(l[6])])
+
+    partsupp_split = spark_context.textFile(
+        DATA_PATH+"partsupp.tbl").map(lambda l: l.split('|'))
+
+    partsupp = partsupp_split.map(
+        lambda l: [int(l[0]), int(l[1]), int(l[2]), float(l[3]), str(l[4])])
+
+    customer_split = spark_context.textFile(
+        DATA_PATH+"customer.tbl").map(lambda l: l.split('|'))
+
+    customer = customer_split.map(lambda l: [int(l[0]), str(l[1]), str(l[2]), int(
+        l[3]), str(l[4]), float(l[5]), str(l[6]), str(l[7])])
+
+    orders_split = spark_context.textFile(
+        DATA_PATH+"orders.tbl").map(lambda l: l.split('|'))
+
+    orders = orders_split.map(lambda l: [int(l[0]), int(l[1]), str(l[2]), float(l[3]), datetime.strptime(
+        l[4], '%Y-%m-%d'), str(l[5]), str(l[6]), int(l[7]), str(l[8])])
+
+    nation_split = spark_context.textFile(
+        DATA_PATH+"nation.tbl").map(lambda l: l.split('|'))
+
+    nation = nation_split.map(
+        lambda l: [int(l[0]), str(l[1]), int(l[2]), str(l[3])])
+
+    region_split = spark_context.textFile(
+        DATA_PATH+"region.tbl").map(lambda l: l.split('|'))
+
+    region = region_split.map(lambda l: [int(l[0]), str(l[1]), str(l[2])])
+
     df_lineitem = spark_session.createDataFrame(lineitem, schema_lineitem)
     df_lineitem.registerTempTable("lineitem")
     df_lineitem.cache().count()
 
+    df_part = spark_session.createDataFrame(part, schema_part)
+    df_part.registerTempTable("part")
+    df_part.cache().count()
 
-def run_query(query, sc, query_name, result_path):
-    result_file = Path(result_path + 'result_' + query_name)
-    if not result_file.is_file():
-        result_file.touch()
+    df_supplier = spark_session.createDataFrame(supplier, schema_supplier)
+    df_supplier.registerTempTable("supplier")
+    df_supplier.cache().count()
 
-    with result_file.open('a') as f:
+    df_partsupp = spark_session.createDataFrame(partsupp, schema_partsupp)
+    df_partsupp.registerTempTable("partsupp")
+    df_partsupp.cache().count()
+
+    df_customer = spark_session.createDataFrame(customer, schema_customer)
+    df_customer.registerTempTable("customer")
+    df_customer.cache().count()
+
+    df_orders = spark_session.createDataFrame(orders, schema_orders)
+    df_orders.registerTempTable("orders")
+    df_orders.cache().count()
+
+    df_nation = spark_session.createDataFrame(nation, schema_nation)
+    df_nation.registerTempTable("nation")
+    df_nation.cache().count()
+
+    df_region = spark_session.createDataFrame(region, schema_region)
+    df_region.registerTempTable("region")
+    df_region.cache().count()
+
+
+def run_query(query, sc, query_name, result_path, open_mode='w+'):
+    result_file = Path(result_path + query_name + '.answer')
+
+    with result_file.open(open_mode) as f:
         start = time()
         query_result = sc.sql(query)
         end = time()
-        f.write(str(query_result))
-        f.write("\n{} result time: {} seconds".format(query_name, (end-start)))
+
+        for idx, d in enumerate(query_result.collect()):
+            if idx == 0:
+                header = ''
+                for r in d.asDict().keys():
+                    header = header + r + ' | '
+                f.write(header + '\n')
+            row_record = ''
+            for v in d.asDict().values():
+                row_record = row_record + str(v) + ' | '
+            f.write(row_record + '\n')
+
+        f.write("\n{} query time: {} seconds\n".format(query_name, (end-start)))
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', '-q', type=str,
+                        default='q1', required=True, help='query number')
+    args = parser.parse_args()
+    query_select = args.query
+
     DATA_PATH = '/home/ruiliu/Development/tpch-pyspark/dbgen/'
-    RESULT_PATH = '/home/ruiliu/Development/tpch-pyspark/query_results/'
+    RESULT_PATH = '/home/ruiliu/Development/tpch-pyspark/answers/'
     spark_sess, spark_ctx = conf_setup()
+
     read_table(spark_sess, spark_ctx)
 
-    run_query(q1.query, spark_sess, "q1", RESULT_PATH)
+    exec_query = globals()[query_select].query
+
+    if isinstance(exec_query, list):
+        for idx, query in enumerate(exec_query):
+            if idx == 0:
+                run_query(query, spark_sess, query_select, RESULT_PATH)
+            else:
+                run_query(query, spark_sess, query_select, RESULT_PATH, 'a+')
+    else:
+        run_query(exec_query, spark_sess, query_select, RESULT_PATH)
--- tpch_schema.py
+++ tpch_schema.py
@@ -7,6 +7,59 @@ from pyspark.sql.types import (
     DateType
 )
 
+schema_part = StructType([
+    StructField("P_PARTKEY", IntegerType(), nullable=False),
+    StructField("P_NAME", StringType(), True),
+    StructField("P_MFGR", StringType(), True),
+    StructField("P_BRAND", StringType(), True),
+    StructField("P_TYPE", StringType(), True),
+    StructField("P_SIZE", IntegerType(), True),
+    StructField("P_CONTAINER", StringType(), True),
+    StructField("P_RETAILPRICE", DoubleType(), True),
+    StructField("P_COMMENT", StringType(), True)
+])
+
+schema_supplier = StructType([
+    StructField("S_SUPPKEY", IntegerType(), nullable=False),
+    StructField("S_NAME", StringType(), True),
+    StructField("S_ADDRESS", StringType(), True),
+    StructField("S_NATIONKEY", IntegerType(), False),
+    StructField("S_PHONE", StringType(), True),
+    StructField("S_ACCTBAL", DoubleType(), True),
+    StructField("S_COMMENT", StringType(), True)
+])
+
+schema_partsupp = StructType([
+    StructField("PS_PARTKEY", IntegerType(), nullable=False),
+    StructField("PS_SUPPKEY", IntegerType(), False),
+    StructField("PS_AVAILQTY", IntegerType(), True),
+    StructField("PS_SUPPLYCOST", DoubleType(), True),
+    StructField("PS_COMMENT", StringType(), True)
+])
+
+schema_customer = StructType([
+    StructField("C_CUSTKEY", IntegerType(), nullable=False),
+    StructField("C_NAME", StringType(), True),
+    StructField("C_ADDRESS", StringType(), True),
+    StructField("C_NATIONKEY", IntegerType(), False),
+    StructField("C_PHONE", StringType(), True),
+    StructField("C_ACCTBAL", DoubleType(), True),
+    StructField("C_MKTSEGMENT", StringType(), True),
+    StructField("C_COMMENT", StringType(), True),
+])
+
+schema_orders = StructType([
+    StructField("O_ORDERKEY", IntegerType(), nullable=False),
+    StructField("O_CUSTKEY", IntegerType(), False),
+    StructField("O_ORDERSTATUS", StringType(), True),
+    StructField("O_TOTALPRICE", DoubleType(), True),
+    StructField("O_ORDERDATE", DateType(), True),
+    StructField("O_ORDERPRIORITY", StringType(), True),
+    StructField("O_CLERK", StringType(), True),
+    StructField("O_SHIPPRIORITY", IntegerType(), True),
+    StructField("O_COMMENT", StringType(), True)
+])
+
 schema_lineitem = StructType([
     StructField("L_ORDERKEY", IntegerType(), nullable=False),
     StructField("L_PARTKEY", IntegerType(), False),
@@ -24,4 +77,17 @@ schema_lineitem = StructType([
     StructField("L_SHIPINSTRUCT", StringType(), True),
     StructField("L_SHIPMODE", StringType(), True),
     StructField("L_COMMENT", StringType(), True)
+])
+
+schema_nation = StructType([
+    StructField("N_NATIONKEY", IntegerType(), nullable=False),
+    StructField("N_NAME", StringType(), True),
+    StructField("N_REGIONKEY", IntegerType(), False),
+    StructField("N_COMMENT", StringType(), True)
+])
+
+schema_region = StructType([
+    StructField("R_REGIONKEY", IntegerType(), nullable=False),
+    StructField("R_NAME", StringType(), True),
+    StructField("R_COMMENT", StringType(), True)
 ])
